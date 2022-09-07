e3b4453009ea4dd463d1ea92c52708f9bb3f60e9
Andreas Ntokas <ntokas.an@gmail.com>
2022-09-07 14:09:21 +0300

    Fixed minor file errors

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Container from './components/Container/Container';
-import '/src/index.css';
+import '/src/Index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
